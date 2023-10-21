3c15ca95929edc6692e9a5eff1cb0134e0decb8b
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-10-21 21:46:22 +0900

    chore(fix): highlight theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  modules: ['@vueuse/nuxt', '@nuxt/content', '@nuxtjs/tailwindcss'],
   content: {
     documentDriven: true,
     highlight: {
-      theme: {
-        // Default theme (same as single string)
-        default: 'github-light',
-        // Theme used if `html.dark`
-        dark: 'github-dark',
-        // Theme used if `html.sepia`
-        sepia: 'monokai'
-      }
+      theme: 'monokai'
     }
   }
 })
