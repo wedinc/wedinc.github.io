8c3eafd1aca9a1beedf9b42b8762c532482607ba
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-10-21 21:46:56 +0900

    feat: clipboard copy from code block

---
+++ components/content/ProseCode.vue
@@ -0,0 +1,29 @@
+<template>
+  <code
+    class="text-sm sm:text-base inline-flex text-left items-center space-x-4 bg-gray-900 text-white rounded-lg p-4 pl-6 relative w-full"
+  >
+    <slot />
+    <img
+      src="@/assets/images/copy.svg"
+      class="absolute w-7 top-2 right-2 bg-gray-600 hover:bg-gray-500 rounded-xl p-1 box-content transition-colors cursor-pointer"
+      @click="onClick"
+    />
+  </code>
+</template>
+
+<script setup lang="ts">
+  import { useClipboard } from '@vueuse/core'
+
+  const props = defineProps({
+    code: {
+      type: String,
+      default: ''
+    }
+  })
+
+  const { copy } = useClipboard()
+
+  const onClick = () => {
+    copy(props.code)
+  }
+</script>
--- pages/[slug].vue
+++ pages/[slug].vue
@@ -1,10 +1,15 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentRenderer v-if="data" :value="data" />
   </main>
 </template>
 
 <script setup lang="ts">
+  const route = useRoute()
+  const { data } = await useAsyncData('get-document', () =>
+    queryContent(route.path).findOne()
+  )
+
   const { page } = useContent()
   useContentHead(page)
 </script>
