edc23a96e337f47c58ccd7160862983590a5ba43
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-10-21 21:59:35 +0900

    chore: add gatag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@vueuse/nuxt', '@nuxt/content', '@nuxtjs/tailwindcss'],
+  modules: [
+    '@vueuse/nuxt',
+    '@nuxt/content',
+    ['@nuxtjs/google-gtag', { id: 'G-8E7TW1CHNY' }],
+    '@nuxtjs/tailwindcss'
+  ],
   content: {
     documentDriven: true,
     highlight: {
