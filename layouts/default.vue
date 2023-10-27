2aae515d92f4c0e4dd406b6774ed03db2cd80265
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-10-27 12:57:53 +0900

    fix: rm footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,5 @@
   <div>
     <p class="text-3xl font-bold underline">WED Engineering Team Blog</p>
     <slot />
-    <footer class="text-3xl font-bold underline">
-      <a href="https://github.com/wedinc" target="_blank">
-        <img class="h-5" src="@/github-mark.png" alt="github logo" />
-      </a>
-    </footer>
   </div>
 </template>
