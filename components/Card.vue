3be7dcf7b05fc6543ea51fb4d4db3d436ee97ab7
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-10-29 22:03:59 +0900

    feat: ogp

--- components/Card.vue
+++ components/Card.vue
@@ -5,7 +5,7 @@
   >
     <img
       class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-      :src="props.image ? `thumbnails/${props.image}` : 'wed-logo.png'"
+      :src="props.image"
       :alt="props.title"
     />
     <div class="flex flex-col justify-between p-4 leading-normal">
--- pages/[slug].vue
+++ pages/[slug].vue
@@ -6,5 +6,26 @@
 
 <script setup lang="ts">
   const { page } = useContent()
+  const thumbnail = page.value.image
+    ? `thumbnails/${page.value.image}`
+    : 'wed-logo.png'
+  useHead({
+    meta: [
+      {
+        name: 'og:image',
+        content: thumbnail
+      },
+      {
+        name: 'twitter:card',
+        content: thumbnail
+      }
+    ]
+  })
   useContentHead(page)
+  // const route = useRoute()
+  // const { data } = await useAsyncData('get-document', () =>
+  //   queryContent(route.path).findOne()
+  // )
+
+  // console.log({ data, page })
 </script>
