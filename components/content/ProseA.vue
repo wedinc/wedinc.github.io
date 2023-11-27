bdbd2a6b08b3a89d8eeb11f73e7be5fc95985325
Naoya Yasuda <43776161+yanskun@users.noreply.github.com>
2023-11-27 18:44:16 +0900

    style(fix): anker link icon, default target blank

---
+++ components/content/ProseA.vue
@@ -0,0 +1,25 @@
+<template>
+  <NuxtLink :href="href" :target="target" class="flex">
+    <slot />
+    <div>
+      <img
+        src="@/assets/images/link.svg"
+        class="w-3 m-0 ml-1 mb-1.5 inline align-bottom"
+      />
+    </div>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    href: {
+      type: String,
+      default: ''
+    },
+    target: {
+      type: String,
+      default: '_blank',
+      required: false
+    }
+  })
+</script>
