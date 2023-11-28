ef636f91ff91adda73feecbb1249944fe7b2ed7d
Abel Toy <abel.toy@wed.company>
2023-11-28 13:17:07 +0900

    Improve article display

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -1,7 +1,14 @@
 <template>
   <main>
     <article class="prose max-w-full">
-      <ContentRenderer v-if="data" :value="data" />
+      <ContentRenderer v-if="data" :value="data" tag="article">
+        <header>
+          <h1>{{ data.title }}</h1>
+          <address class="author">{{ data.author }}</address>
+          <time :datetime="$dayjs(data.date).format('YYYY-MM-DD')">{{ $dayjs(data.date).format('LL') }}</time>
+        </header>
+        <ContentRendererMarkdown :value="data" />
+      </ContentRenderer>
     </article>
   </main>
 </template>
